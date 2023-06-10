69c21a355b488a14d95b627ae36004047f70a90b
Ângelo <angelo.pralves@gmail.com>
2023-06-10 17:35:36 -0300

    UserInterface: use of JFrame to display info

--- src/domain/impl/UserInterface.java
+++ src/domain/impl/UserInterface.java
@@ -4,9 +4,19 @@ import domain.api.ControlInterface;
 import domain.api.NotificationInterface;
 import domain.api.SubmissionInterface;
 
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 public class UserInterface implements NotificationInterface {
     private ControlInterface controlInterface;
     private SubmissionInterface submissionInterface;
+
+    private JTextArea infoDisplayText;
+
+    public UserInterface() {
+        createFrame();
+    }
 
     public void setControlInterface(ControlInterface controlInterface) {
         this.controlInterface = controlInterface;
@@ -16,10 +26,26 @@ public class UserInterface implements NotificationInterface {
         this.submissionInterface = submissionInterface;
     }
 
+    /**
+     * Method used by the schedulers to notify any information related to the simulation.
+     * The message is displayed like "INFO: [info]" through the opened notifications JFrame.
+     *
+     * @param info The information string to be displayed.
+     */
     @Override
     public void display(String info) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'display'");
+        infoDisplayText.append("INFO: " + info + "\n"); // Appends new text info in the display component
+        infoDisplayText.setCaretPosition(infoDisplayText.getDocument().getLength()); // Scroll to bottom
+    
+
+    private void createFrame() {
+        var infoDisplayFrame = new JFrame("Simulation Notifcations");
+        infoDisplayText = new JTextArea(10, 30);
+        var scrollPane = new JScrollPane(infoDisplayText);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        infoDisplayFrame.getContentPane().add(scrollPane);
+        infoDisplayFrame.setSize(700, 900);
+        infoDisplayFrame.setVisible(true);
     }
 
 }
