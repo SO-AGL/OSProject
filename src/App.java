eaf8f6b660506193b6c865f01febde9b436e1fbb
Ângelo <angelo.pralves@gmail.com>
2023-06-20 19:20:06 -0300

    app: apply suggested changes

--- src/App.java
+++ src/App.java
@@ -7,8 +7,9 @@ import domain.impl.ShortestJobFirst;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int npIndex = Arrays.asList(args).indexOf("-np"); // index of the number of process flag in args array
-        int stratIndex = Arrays.asList(args).indexOf("-strat"); // index of the strategy flag in args array (rr or sjf)
+        var argsList = Arrays.asList(args); // convert arguments array to list to facilitate finding the indexes of the flags
+        int npIndex = argsList.indexOf("-np"); // index of the number of process flag in args array
+        int stratIndex = argsList.indexOf("-strat"); // index of the strategy flag in args array (rr or sjf)
         int maxProcessReadySize = 100; // default max number of processes in the ready queue
         String stratName = "sjf"; // default strategy: SJF
         SchedulingStrategy strategy;
@@ -20,32 +21,30 @@ public class App {
                     throw new NumberFormatException();
                 }
             } catch (Exception e) {
-                if (e instanceof ArrayIndexOutOfBoundsException) { // if the user uses the flag and don't inform an value in sequence
-                    System.err.println("If using -np flag, inform an value (>0)");
-                    return;
+                if (e instanceof ArrayIndexOutOfBoundsException) { // if the user uses the flag and don't inform a value in sequence
+                    throw new ArrayIndexOutOfBoundsException("If using -np flag, inform a value (>0)");
                 }
                 if (e instanceof NumberFormatException) { // if the value informed wasn't valid (couldn't convert to int, or <=0)
-                    System.err.println("Inform an valid value for np (>0)");
-                    return;
+                    throw new NumberFormatException("Inform a valid value for np (>0)");
                 }
+                throw e;
             }
         }
 
-        if (stratIndex != -1) {
+        if (stratIndex != -1) { // the -strat flag was used
             try {
                 stratName = args[stratIndex+1];
                 if (!stratName.equals("sjf") && !stratName.equals("rr")) {
                     throw new IllegalArgumentException();
                 }
             } catch (Exception e) {
-                if (e instanceof ArrayIndexOutOfBoundsException) { // if the user uses the flag and don't inform an value in sequence
-                    System.err.println("If using -strat flag, inform an value ('rr' or 'sjf')");
-                    return;
+                if (e instanceof ArrayIndexOutOfBoundsException) { // if the user uses the flag and don't inform a value in sequence
+                    throw new ArrayIndexOutOfBoundsException("If using -strat flag, inform a value ('rr' or 'sjf')");
                 }
                 if (e instanceof IllegalArgumentException) { // if the value informed wasn't valid (not 'sjf' nor 'rr')
-                    System.err.println("Inform an valid strategy ('rr' or 'sjf')");
-                    return;
+                    throw new IllegalArgumentException("Inform a valid strategy ('rr' or 'sjf')");
                 }
+                throw e;
             }
         }
 
